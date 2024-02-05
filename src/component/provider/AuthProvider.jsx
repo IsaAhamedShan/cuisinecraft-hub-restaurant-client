6e0d742ba273771540c6fcb762c59d3fc2a36a95
isa <isaahmedshan190138@gmail.com>
2024-02-06 01:58:08 +0600

    created jwt token and saved to ls

--- src/component/provider/AuthProvider.jsx
+++ src/component/provider/AuthProvider.jsx
@@ -7,10 +7,12 @@ import {
   signOut,
 } from "firebase/auth";
 import { auth } from "../firebase/firebase.config";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
+  const axiosPublic = useAxiosPublic();
   const [user, setUser] = useState(null);
   const register = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -24,6 +26,17 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, currentUser => {
       setUser(currentUser);
+      if (currentUser) {
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then(res => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+            
+          }
+        });
+      } else {
+        localStorage.removeItem("access-token");
+      }
       console.log("Current User: ", user);
     });
     return () => {
