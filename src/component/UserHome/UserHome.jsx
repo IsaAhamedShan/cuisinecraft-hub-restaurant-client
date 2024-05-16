02c6a03e73acaae508e8f1ad73ae6700a56d4378
isa <isaahmedshan190138@gmail.com>
2024-05-16 20:45:27 +0600

    gg

--- src/component/UserHome/UserHome.jsx
+++ src/component/UserHome/UserHome.jsx
@@ -1,28 +1,50 @@
 import { useQuery } from "@tanstack/react-query";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GrMoney } from "react-icons/gr";
-import { IoIosPeople } from "react-icons/io";
 import { MdFastfood } from "react-icons/md";
 import { TbTruckDelivery } from "react-icons/tb";
-import AdminHomePieChart from "../AdminHome/AdminHomePieChart";
 import AdminHomeTowerChart from "../AdminHome/AdminHomeTowerChart";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
 import { AuthContext } from "../provider/AuthProvider";
+import UserHomePieChart from "./UserHomePieChart";
 
 const UserHome = () => {
   const { user } = useContext(AuthContext);
-
+  const [totalExpenses, setTotalExpenses] = useState(
+    <div className="flex justify-center items-center">
+      <span className="loading loading-spinner loading-md"></span>
+    </div>
+  );
+  const [totalItem, setTotalItem] = useState(<div className="flex justify-center items-center">
+  <span className="loading loading-spinner loading-md"></span>
+</div>);
   const axiosSecure = useAxiosSecure();
   const { data } = useQuery({
-    queryKey: ["admin-stats",user],
+    queryKey: ["admin-stats", user],
     queryFn: async () => {
       let responseAdminStats = await axiosSecure.get("admin-stats");
-      let responseSoldStats = await axiosSecure.get(`/userPaymentHistory?email=${user?.email}`);
+      let responseSoldStats = await axiosSecure.get(
+        `/userPaymentHistory?email=${user?.email}`
+      );
       console.log(
         "data in admin home: ",
         responseAdminStats.data,
         responseSoldStats.data
       );
+
+      setTotalExpenses(
+        responseSoldStats.data.reduce(
+          (sum, current) => (sum += current.revenue),
+          0
+        )
+      );
+      setTotalItem(
+        responseSoldStats.data.reduce(
+          (sum, current) => (sum += current.quantity),
+          0
+        )
+      );
+
       return [responseSoldStats.data, responseAdminStats.data];
     },
   });
@@ -40,22 +62,10 @@ const UserHome = () => {
               <div className="stat-figure text-secondary">
                 <GrMoney className="text-3xl text-blue-500" />
               </div>
-              <div className="stat-title">Revenue</div>
-              <div className="stat-value font-normal">
-                {data && data[1].revenue}
-              </div>
-              {/* <div className="stat-desc">Jan 1st - Feb 1st</div> */}
+              <div className="stat-title">Total Expenses</div>
+              <div className="stat-value font-normal">{totalExpenses}</div>
             </div>
-            <div className="stat">
-              <div className="stat-figure text-secondary">
-                <IoIosPeople className="text-3xl text-blue-500" />
-              </div>
-              <div className="stat-title">Users</div>
-              <div className="stat-value font-normal">
-                {data && data[1].users}
-              </div>
-              {/* <div className="stat-desc">↗︎ 400 (22%)</div> */}
-            </div>
+
             <div className="stat">
               <div className="stat-figure text-secondary">
                 <TbTruckDelivery className="text-3xl text-blue-500" />
@@ -70,9 +80,9 @@ const UserHome = () => {
               <div className="stat-figure text-secondary">
                 <MdFastfood className="text-3xl text-blue-500" />
               </div>
-              <div className="stat-title">Items</div>
+              <div className="stat-title">Total items</div>
               <div className="stat-value font-normal">
-                {data && data[1].menuItems}
+                {totalItem}
               </div>
               {/* <div className="stat-desc">↘︎ 90 (14%)</div> */}
             </div>
@@ -82,20 +92,20 @@ const UserHome = () => {
       {/* charts */}
       <div className="flex justify-center items-center flex-wrap gap-6  md:gap-8 md:mt-20 lg:gap-12">
         <div className="flex flex-col gap-4 justify-center items-center mt-6 md:mt-0">
-          <h2 className="text-2xl text-blue-600 "></h2>
-        {
-          data && 
-        <AdminHomePieChart soldData = {data[0]} totalRevenue = {data[1].revenue}></AdminHomePieChart>
-        }
-
+          <h2 className="text-2xl text-blue-600 ">Expenses</h2>
+          {data && (
+            <UserHomePieChart
+              soldData={data[0]}
+              ExpensePerCategory={data[0]}
+            ></UserHomePieChart>
+          )}
         </div>
         <div className="flex flex-col gap-4 justify-center items-center">
-          <h2 className="text-2xl text-blue-600 " >Brought Items</h2>
-        {
-          data && <AdminHomeTowerChart soldData = {data[0]}></AdminHomeTowerChart>
-        }
+          <h2 className="text-2xl text-blue-600 ">Brought Items</h2>
+          {data && (
+            <AdminHomeTowerChart soldData={data[0]}></AdminHomeTowerChart>
+          )}
         </div>
-       
       </div>
     </>
   );
